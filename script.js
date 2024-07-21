afe6172499ff1790ede3c20c0ea5ffade5eed6f6
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-21 20:32:25 -0300

    feat: create variables

---
+++ script.js
@@ -0,0 +1,7 @@
+const btnCriptografar = document.getElementById('btnCriptografar');
+const btnDescriptografar = document.getElementById('btnDescriptografar');
+const btnCopiar = document.getElementById('btnCopiar');
+const areaTexto = document.getElementById('areaTexto');
+const resultado = document.querySelector('.resultado');
+const text = document.querySelector('.text');
+const banner = document.querySelector('.banner');
