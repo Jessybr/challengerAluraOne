3a1ae4a61aac8175edfdef00c242f9945e45d07f
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-22 14:44:22 -0300

    feat: add variable

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ const areaTexto = document.getElementById('areaTexto');
 const resultado = document.querySelector('.resultado');
 const text = document.querySelector('.text');
 const banner = document.querySelector('.banner');
+const acentos = ['á','à','é','è','í','ì','ó','ò','ú','ù','ü','ã','õ','ê','ä','ë','ï','ö'];
+
